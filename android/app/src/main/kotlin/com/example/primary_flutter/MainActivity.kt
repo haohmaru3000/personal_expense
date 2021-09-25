7cd00af65884c87388d4589ea33e89829e406afa
tuanit168 <tuanit168@gmail.com>
2021-09-26 04:04:03 +1000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/primary_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.primary_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favourite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Primary Flutter Project'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              child: Text(
+                'What\'s your favourite color?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
