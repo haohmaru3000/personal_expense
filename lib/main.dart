3e41796c4927ef9fbd84d1b54b73f0469ff6e7ce
tuanit168 <tuanit168@gmail.com>
2021-09-29 10:03:14 +1000

    Add titleController and amountController

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,11 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,15 +62,22 @@ class MyHomePage extends StatelessWidget {
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (val) {
+                    //   titleInput = val;
+                    // },
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    // onChanged: (val) => amountInput = val,
                   ),
                   FlatButton(
                     child: Text('Add Transaction'),
                     textColor: Colors.purple,
                     onPressed: () {
-
+                      print(titleController.text);
+                      print(amountController.text);
                     },
                   )
                 ],
