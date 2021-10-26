161c5721e1980ca43deb224d6f743b575c47b76e
tuanit168 <tuanit168@gmail.com>
2021-10-26 12:28:15 +1100

    Complete fixing landscape mode problem

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,61 +10,66 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return transactions.isEmpty ? Column(
-      children: <Widget>[
-        Text(
-          'No transactions added yet!',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          height: 200,
-          child: Image.asset(
-            'assets/images/waiting.png',
-            fit: BoxFit.cover,
-          ),
-        ),
-      ],): ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            elevation: 5,
-            margin: EdgeInsets.symmetric(
-              vertical: 8,
-              horizontal: 5,
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                  padding: EdgeInsets.all(6),
-                  child: FittedBox(
-                    child: Text(
-                      '\$${transactions[index].amount}',
-                    ),
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-              title: Text(
-                transactions[index].title,
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              subtitle: Text(
-                DateFormat.yMMMd().format(transactions[index].date),
-              ),
-              trailing: IconButton(
-                icon: Icon(Icons.delete),
-                color: Theme.of(context).colorScheme.error,
-                onPressed: () => deleteTx(transactions[index].id),
-              ),
-            ),
+              ],
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text(
+                          '\$${transactions[index].amount}',
+                        ),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).colorScheme.error,
+                    onPressed: () => deleteTx(transactions[index].id),
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions
+                .length, // if we have 2 transactions, we're gonna build 2 items
+            // children: transactions.map((tx) {
+            // }).toList(),
           );
-        },
-        itemCount: transactions
-            .length, // if we have 2 transactions, we're gonna build 2 items
-        // children: transactions.map((tx) {
-        // }).toList(),
-      );
   }
 }
